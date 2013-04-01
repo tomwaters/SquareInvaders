fa46914b41fe5a6dc525484545f89e1b8d9bd8a9
Tom Waters <github@random-works.co.uk>
2013-04-01 21:40:58 +0100

    Several Changes
    Multiple enemies in a wave, new wave after a wave is killed, each new
    wave is faster than the previous, game over if enemies reach the bottom
    of the screen.

--- enemies.js
+++ enemies.js
@@ -1,16 +1,68 @@
+function enemies() {
+	this.w = 20;
+	this.h = 20;
+	this.s = 2;
+	this.init();
+}
+
+enemies.prototype.init = function() {
+	this.e = new Array();
+	this.y = 0;
+	this.dir = "r";
+	this.pauseFrames = 30;
+	
+	this.numEs = 10;
+	for(var i=0; i<this.numEs; i++) {
+		var newE = new enemy(i*(this.w + 5), 0, this.w, this.h);
+		this.e.push(newE);
+	}
+}
+
+enemies.prototype.draw = function(ctx, player) {
+	if(this.y >= ctx.canvas.height - this.h) {
+		player.state = "dead";
+	} else if(this.e.length == 0) {
+		if(this.pauseFrames == 0) {
+			this.init();
+			this.s = this.s + 2;
+		} else {
+			this.pauseFrames = this.pauseFrames - 1;
+		}
+	} else {
+		for(var i=this.e.length - 1; i>-1; i--) {
+			this.e[i].draw(ctx, player, this.y);
+			
+			if(this.e[i].deadFrames == 0) {
+				this.e.splice(i, 1);
+			} else if(this.dir == "r") {
+				this.e[i].x = this.e[i].x + this.s;
+			} else {
+				this.e[i].x = this.e[i].x - this.s;
+			}		
+		}
+
+		var last = this.e[this.e.length - 1];
+		if(last.x + last.width > ctx.canvas.width) {
+			this.dir = "l";
+			this.y = this.y + this.h;
+		} else if (this.e[0].x <= 0) {
+			this.dir = "r";
+			this.y = this.y + this.h;
+		}
+	}
+}
+
 function enemy(x, y, width, height) {
 	this.width = width;
 	this.height = height;
 	this.x = x;
-	this.y = y;
-	this.dir = "r";
 	this.state = "alive";
 	this.deadFrames = 30;
 }
 
-enemy.prototype.draw = function(ctx, player) {
+enemy.prototype.draw = function(ctx, player, y) {
 	for(var i = player.shots.length - 1; i>-1; i = i - 1) {
-		if((player.shots[i].y - player.shotLength < this.y + this.height) && 
+		if((player.shots[i].y - player.shotLength < y + this.height) && 
 			(player.shots[i].x >= this.x) && (player.shots[i].x <= this.x + this.width) && 
 			(this.state == "alive")) {
 				this.state = "dead";
@@ -21,26 +73,11 @@ enemy.prototype.draw = function(ctx, player) {
 	
 	if(this.state == "alive") {
 		ctx.fillStyle = "blue";
-		ctx.fillRect(this.x, this.y, this.width, this.height);
-		this.move(ctx);
+		ctx.fillRect(this.x, y, this.width, this.height);
 	} else if(this.deadFrames > 0){
 		this.deadFrames = this.deadFrames - 1;
 		ctx.fillStyle = "red";
-		ctx.fillRect(this.x, this.y, this.width, this.height);
-	}
+		ctx.fillRect(this.x, y, this.width, this.height);
+	} 
 }
 
-enemy.prototype.move = function(ctx) {
-	if(this.x + this.width > ctx.canvas.width) {
-		this.dir = "l";
-	} else if (this.x == 0) {
-		this.dir = "r";
-	}
-	
-	if(this.dir == "r") {
-		this.x = this.x + 2;
-	} else {
-		this.x = this.x - 2;
-	}
-	
-}
--- player.js
+++ player.js
@@ -8,36 +8,54 @@ function player(x, y, width, height, maxShots) {
 	this.shotLength = 10;
 	this.pStep = 10;
 	this.score = 0;
+	this.state = "alive";
+	this.deadFrames = 30;
 }
 
 player.prototype.draw = function(ctx) {
-	ctx.fillStyle = "green";
-	ctx.fillRect(this.x, this.y, this.width, this.height);
+	if(this.state == "alive") {
+		ctx.fillStyle = "green";
+		ctx.fillRect(this.x, this.y, this.width, this.height);
 	
-	ctx.beginPath();
-	for(var i = this.shots.length - 1; i>-1; i = i - 1) {
-		var value = this.shots[i];
-		ctx.moveTo(value.x, value.y);
-		ctx.lineTo(value.x, value.y - this.shotLength);
-		ctx.stroke();
-		if(value.y > 0)
-			value.y = value.y - 4;
-		else
-			this.shots.splice(i, 1);
+		ctx.beginPath();
+		for(var i = this.shots.length - 1; i>-1; i = i - 1) {
+			var value = this.shots[i];
+			ctx.moveTo(value.x, value.y);
+			ctx.lineTo(value.x, value.y - this.shotLength);
+			ctx.stroke();
+			if(value.y > 0)
+				value.y = value.y - 4;
+			else
+				this.shots.splice(i, 1);
+		}
+	} else if(this.deadFrames > 0) {
+		ctx.fillStyle = "red";
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+		this.deadFrames = this.deadFrames - 1;
+	} else {
+		ctx.font = "50px Verdana";
+		ctx.fillStyle = "red";
+		ctx.fillText("Game Over!", (ctx.canvas.width / 2) - 150, ctx.canvas.height / 2);
 	}
 }
 
 player.prototype.fire = function() {
+	if(this.state == "alive") {
 		var s = new Object();
 		s.x = this.x + (this.width / 2);
 		s.y = this.y - this.height;
 		p.shots.push(s);
+	}
 }
 
 player.prototype.moveLeft = function() {
-	this.x = this.x - p.pStep;
+	if(this.state == "alive") {
+		this.x = this.x - p.pStep;
+	}
 }
 
 player.prototype.moveRight = function() {
-	this.x = this.x + p.pStep;
+	if(this.state == "alive") {
+		this.x = this.x + p.pStep;
+	}
 }
